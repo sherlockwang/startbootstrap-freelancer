9a034c6172befce66203e01b15bec99999d660f8
Jinghan Wang <jwang@beancreative.com>
2016-08-25 17:16:41 -0400

    add linkedin link

--- gulpfile.js
+++ gulpfile.js
@@ -94,7 +94,7 @@ gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() 
 });
 
 // Build task
-gulp.task('build', ['less', 'minify-css', 'minify-js'], function () {
+gulp.task('build', ['browserSync', 'less', 'minify-css', 'minify-js'], function () {
     gulp.src('index.html')
         .pipe(gulp.dest('dist'));
     gulp.src('vendor/**/*')
